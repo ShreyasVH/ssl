3ccba8114a89f81eba314bd38fe86fca2d6f2e98
ShreyasVH <shreyas.deve.1@gmail.com>
2025-05-14 21:01:55 +0530

    updates

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,6 @@
 openssl x509 -req -in server.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
+rm server.pem
+touch server.pem
 cat server.crt >> server.pem
 echo "" >> server.pem
 cat server.key >> server.pem
